fd7bc2bf71ea235106c7685fc026cfc064e98f1b
mafazann <113650768+mafazann@users.noreply.github.com>
2023-08-29 16:11:41 +0700

    Add files via upload

---
+++ week-01/README.md
@@ -0,0 +1,5 @@
+# 2141720140-mobile-2023
+# M. Rizky Mafazan
+# 3F
+# 16
+![Screenshot hello_world](docs/hello_world.png)
---
+++ week-01/src/main.dart
@@ -0,0 +1,6 @@
+void main() {
+  print('Hello, World!');
+  print('Nama : M. Rizky Mafazan');
+  print('Kelas : TI-3F');
+  print('NIM : 2141720140');
+}
